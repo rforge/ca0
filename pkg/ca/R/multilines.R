e934f27f8af1ed1c528dbb05fe5ce53484d79579
friendly <friendly@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-01-28 19:49:07 +0000

    add which= argument to multilines
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ca0@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ca/R/multilines.R
+++ pkg/ca/R/multilines.R
@@ -1,13 +1,14 @@
 # plot with multi lines by group 
 
-multilines <- function(XY, group=NULL, sort=1, type='l', col=palette(), lwd=1, ...) {
+multilines <- function(XY, group=NULL, which=1:nf, sort=1, type='l', col=palette(), lwd=1, ...) {
   if (is.null(group)) group <- rep(1, nrow(XY))
-  fact <- unique(as.character(group))
+  fact <- as.character(group)
+  fact <- fact[!duplicated(fact)]
   nf <- length(fact)
   col <- rep(col, out.length=nf)
   lwd <- rep(lwd, out.length=nf)
   
-  for (i in seq_along(fact)) {
+  for (i in which) {
     xy <- subset(XY, subset=group==fact[i])
     if (sort %in% 1:2) {
       ord <- order(xy[, sort])
